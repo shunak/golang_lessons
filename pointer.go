b2a0ddf9baf0015531d3391d7408ddd91cf316f8
DESKTOP-BRIGC4V\USER <shuna966@gmail.com>
2018-07-10 23:09:36 +0900

    180710_4

---
+++ pointer.go
@@ -0,0 +1,16 @@
+// ポインタ
+// アドレスを指し示す変数
+// ポインタの演算はできない
+package main
+
+import "fmt"
+
+func main() {
+	a := 5
+	var pa *int
+	pa = &a //&a = aのアドレス
+	// paの領域にあるデータの値 = *pa
+	fmt.Println(pa)
+	fmt.Println(*pa)
+
+}
